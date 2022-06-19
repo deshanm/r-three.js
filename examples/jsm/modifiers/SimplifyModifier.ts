89a2424ab90ba4dbf6125b5104914369cbed999a
deshanm <madurajithdeshan@gmail.com>
2022-06-19 21:54:01 +0530

    typescript: converted to typescript and added type

--- examples/jsm/modifiers/SimplifyModifier.ts
+++ examples/jsm/modifiers/SimplifyModifier.ts
@@ -318,7 +318,14 @@ function removeFace( f, faces ) {
 
 }
 
-function collapse( vertices, faces, u, v ) { // u and v are pointers to vertices of an edge
+interface Faces {
+	neighbors: any[],
+	faces: {
+		hasVertex: (v: any)=> boolean
+	}[],
+}
+
+function collapse( vertices, faces, u: Faces, v ) { // u and v are pointers to vertices of an edge
 
 	// Collapse the edge uv by moving vertex u onto v
 
@@ -330,7 +337,7 @@ function collapse( vertices, faces, u, v ) { // u and v are pointers to vertices
 
 	}
 
-	const tmpVertices = [];
+	const tmpVertices: any[] = [];
 
 	for ( let i = 0; i < u.neighbors.length; i ++ ) {
 
